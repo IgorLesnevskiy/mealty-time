257b6fba4c7030393881536511394e2cec8774e5
Igor Lesnevskiy <lesnevskii.igor@gmail.com>
2020-04-04 17:41:45 +0300

    Updates

---
+++ precache-manifest.735f811869e23e4e6d44afde776759f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbbf64673be9e453405101ac0f5afe1",
+    "url": "/mealty-time/index.html"
+  },
+  {
+    "revision": "bfda75336f7a8748779e",
+    "url": "/mealty-time/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cba36f8abe5b763f9274",
+    "url": "/mealty-time/static/js/2.02c51119.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mealty-time/static/js/2.02c51119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfda75336f7a8748779e",
+    "url": "/mealty-time/static/js/main.1ea79cd4.chunk.js"
+  },
+  {
+    "revision": "9840091daeb54cbdc2c8",
+    "url": "/mealty-time/static/js/runtime-main.17792d70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mealty-time/static/media/logo.5d5d9eef.svg"
+  }
+]);
